13403893b5c8c4abcc4ba1fd4ddb9235f088214a
daniice <daball@alumni.princeton.edu>
2023-03-27 19:29:33 -0400

    first day, figuring out structure

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// how to get order of operations?
+
+createCalc();
+
+// function to make calculator size with screen
+function createCalc() {
+    const container = document.querySelector('#squares');
+
+    for (i=1; i<=num; i++) {
+        const squareRow = document.createElement('div');
+}
+}
